2c35af09dbbb05ce9f5b3bd5319c0d8ebd7b6757
Bennoux <benjid0527@yahoo.fr>
2019-01-17 20:34:18 +0100

    Base CSS / HTML

--- index.php
+++ index.php
@@ -1,17 +1,132 @@
 <!DOCTYPE html>
 <html>
+    <head>
+        <title>Le meilleur sites de fruits</title>
+        <meta charset='utf-8' />
+		<link rel="stylesheet" type="text/css" href="restcss.css" />
+		<link rel="stylesheet" type="text/css" href="simplegrid.css" />
+		<link rel="stylesheet" type="text/css" href="style.css" />
+    </head>
 
-<head>
-  <meta charset="utf-8">
-    <title>Front</title>
-</head>
+    <body>
+        <header class="menu" >
+			<div class="grid grid-pad menu">
+				<div class="col-1-1 menu",>
+					<h1 class="menu">Les Fruits en folie</h1>
+				</div>
+				<nav class="menu">
+					<ul class="menu">
+						<div class="col-1-4 menu">
+							<li class="menu">
+								<a class="menu" href="index.html" title="Home">Acceuil</a>
+							</li>
+						</div>
+						<div class="col-1-4 menu">
+							<li class="menu">
+								<a class="menu" href="index.html" title="Recette"> Nos recettes</a>
+							</li>
+						</div>
+						<div class="col-1-4 menu">
+							<li class="menu">
+								<a class="menu" href="index.html" title="Conseil"> Nos conseils nutritions</a>
+							</li>
+						</div>
+						<div class="col-1-4 menu">
+							<li class="menu">
+								<a class="menu" href="index.html" title="Contact"> Nous Contacter</a>
+							</li>
+						</div>
+					</ul>
+				</nav>
+			</div>
+        </header>
 
-<body>
-    <div id="main">
-      <p>Vous êtes bien sur le front</p>
+        <main>
+			<div class="grid grid-pad">
+				<div class="col-1-1 article">
+				   <header class="font_article">
+						<article>
+							<header class="font_article">
+								<h2 class="font_article">Une decouverte fracassante sur les pommes</h2>
+								<time class="font_article">08/12/2017</time>
+							</header>
 
-    </div>
+							<section class="font_article">
+								<div class="col-1-2 font_article">
+									Plusieurs études montrent que le fait de manger des pommes et les performances sexuelles ont un lien. En effet des chercheurs russes ont montré que ...
+								</div>
+								<div class="col-1-2 font_article">
+								<img class="img_article1" src="article1.jpg">
+								</div>
+							</section>
+							<div class="col-1-1 font_article">
+								<footer class="font_article">
+									<a class="font_article" href="index.html" title="Plus article 1">Lire Plus</a>
+								</footer>
+							</div>
+						</article>
+					</header>
+				</div>
+			<div class="grid grid-pad">
+				<div class="col-8-12">
+					<section>
+						<div class="col-1-2">
+							<article class="article">
+									<header class="font_article">
+										<h2 class="font_article">Le citron un ingredient incroyable</h2>
+										<time class="font_article">07/12/2017</time>
+									</header>
+									<section class="font_article">
+										<div class="col-1-1 font_article">
+											Le citron est un ingrédient méconue qui derrière ces aspects acides cache de nombreux atouts. Ce dernier serait la basse de le plus parti de nos régimes modernes ...
+										</div>
+										<div class="col-1-1 font_article">
+											<img src="article2.jpg">
+										</div>
+									</section>
+									<footer class="font_article">
+										<a class="font_article" href="index.html" title="Plus article 2">Lire Plus</a>
+									</footer>
+								</article>
+						</div>
+						<div class="col-1-2">
+								<article class="article">
+									<header class="font_article">
+										<h2 class="font_article">Les fruits de la saison</h2>
+										<time class="font_article">05/12/2017</time>
+									</header>
+									<section class="font_article">
+										<div class="col-1-1 font_article">
+											Vous ne savez pas quoi manger de bon cet hiver, cet article est fait pour vous, nous y exposerons les fruits qui vous redonneront la pêche pendant cette saison froide. ...
+										</div>
+										<div class="col-1-1 font_article">
+											<img src="article3.jpg">
+										</div>
+									</section>
+									<footer class="font_article">
+											<a class="font_article" href="index.html" title="Plus article 3">Lire Plus</a>
+									</footer>
+								</article>
+						</div>
+					</section>
+				</div>
+				<div class="col-4-12">
+					<aside>
+						<div class="PUB">
+							/!\ Désactive Adblock on est pauvre /!\
+						</div>
+					</aside>
+				</div>
 
-</body>
-
+			</div>
+        </main>
+		<div class="grid grid-pad">
+			<div class="col-1-1">
+				<footer>
+					<p>2017.TP6</p>
+					<p>Pierre-André Kuntzmann<p/>
+				</footer>
+			</div>
+		</div>
+    </body>
 </html>
