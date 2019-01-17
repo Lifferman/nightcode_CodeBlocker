88078e745bd9d1dfaf7812f23d50be63ecc9778c
Joseph Dongny <josef.dongny@gamil.com>
2019-01-17 20:14:51 +0100

    index.php front presque vide

---
+++ default/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+    <title>Front</title>
+</head>
+
+<body>
+    <div id="main">
+      <p>Vous êtes bien sur le front</p>
+
+    </div>
+
+</body>
+
+</html>
