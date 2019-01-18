8df86dab0001747f45d90216e8c70537139a6248
LordDX <sullydu96@gmail.com>
2019-01-18 04:00:17 +0100

    Tentative de message 2

--- messages/index.php
+++ messages/index.php
@@ -1,7 +1,16 @@
-  <?php
-  use GuzzleHttp\Client;
+<?php
+header('Content-Type: application/json');
+$chaine = '"payload" : {
+        "inputs" : [
+            { "uuid": "f81d4fae-7dec-11d0-a765-00a0c91e6bf6", "msg" : "bonjour la terre"},
+            { "uuid": "4fe5d828-6444-11e8-8222-720007e40350", "msg" : "Je suis vivant."}
+        ]
+    }';
+$payload = "payload";
+$input = "inputs";
+$uuid = "uuid";
+$msg = "msg";
 
-  $client = new Client([
-      'base_uri' => 'http://82.165.98.24/back/messages',
-      'timeout'  => 2.0,
-  ]);
+if(isset($payload,$input,$uuid,$msg)){
+  echo json_encode(array('status' => 'Ok'), JSON_NUMERIC_CHECK);
+}
