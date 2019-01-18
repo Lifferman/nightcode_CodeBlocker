7cfddf36b5a15298fe375e6111837fd146ff0daf
LordDX <sullydu96@gmail.com>
2019-01-18 01:02:47 +0100

    Tentative de message

--- index.php
+++ index.php
@@ -1,17 +1,12 @@
 <?php
+use GuzzleHttp\Client;
 
-switch (connection_status())
-{
-case CONNECTION_NORMAL:
-  $txt = 'Status code: 200';
-  break;
-case (CONNECTION_ABORTED & CONNECTION_TIMEOUT):
-  $txt = 'Connexion non reussite';
-  break;
-default:
-  $txt = 'Unknown';
-  break;
-}
-//echo $txt;
-echo "status : Ok";
-?>
+use GuzzleHttp\EntityBody;
+
+$responseLivre = $client->request('POST', 'http://82.165.98.24/back/messages’,
+['form_params' => [
+‘uuid'                     => "f81d4fae-7dec-11d0-a765-00a0c91e6bf6",
+‘msg’                 => "bonjour la terre",
+‘uuid’                  => "4fe5d828-6444-11e8-8222-720007e40350",
+‘msg’  => "Je suis vivant.",
+]]);
--- messages/index.php
+++ messages/index.php
@@ -1,18 +1,7 @@
-<!DOCTYPE html>
-<html lang="fr" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-  </head>
-  <body>
-    <?php
-    "payload" : {
-        "inputs" : [
-            { "uuid": "f81d4fae-7dec-11d0-a765-00a0c91e6bf6", "msg" : "bonjour la terre"},
-            { "uuid": "4fe5d828-6444-11e8-8222-720007e40350", "msg" : "Je suis vivant."}
-        ]
-    }
-    //$data = json_decode($request_body);
-     ?>
-  </body>
-</html>
+  <?php
+  use GuzzleHttp\Client;
+
+  $client = new Client([
+      'base_uri' => 'http://82.165.98.24/back/messages',
+      'timeout'  => 2.0,
+  ]);
